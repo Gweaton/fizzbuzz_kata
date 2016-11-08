ca1a3962aee466153b226846bbd6474af6a93e75
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 09:52:39 +0000

    passes fizz and buzz tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,5 +3,10 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when given a number divisible by 3' do
     expect(fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+  it 'returns "buzz" when given a number divisible by 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+    expect(fizzbuzz(10)).to eq "buzz"
   end
 end
