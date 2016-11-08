788fc2196b044edcab11dc280f76ff170b8406bf
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 10:01:57 +0000

    works for multiples of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,5 +4,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
 
 end
