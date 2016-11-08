5ce14dd9ba60e92fd14099b448b63df0d1e28ca2
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 10:05:44 +0000

    passes all requirements

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,5 +10,10 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
+  it 'returns the number when passed any other number' do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz(97)).to eq 97
+    expect(fizzbuzz(53)).to eq 53
+  end
 
 end
