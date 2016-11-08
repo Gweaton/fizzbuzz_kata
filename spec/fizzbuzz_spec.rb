acbef35196eeeb529421395876781ed31a1e7c8d
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 10:24:00 +0000

    finished, cleared and starting again

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,1 @@
-require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-  it 'returns "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
-  end
-  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz(30)).to eq 'fizzbuzz'
-  end
-  it 'returns the number when passed any other number' do
-    expect(fizzbuzz(1)).to eq 1
-    expect(fizzbuzz(23)).to eq 23
-    expect(fizzbuzz(97)).to eq 97
-  end
-
-end
